a575cca4f2c362454e1c30f627d0d4d6f96b6efd
javedqazi <javedqazi20@gmail.com>
2023-02-23 22:21:09 +0500

    log removed

--- src/components/News.js
+++ src/components/News.js
@@ -76,7 +76,7 @@ export class News extends Component {
     
     }
   render () {
-    console.log("render");
+    
     return (      
         <div className="container my-3">
           <h2>NewsMonkey-Top Headlines</h2>          
